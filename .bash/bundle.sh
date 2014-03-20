eddb6a3cf2328f6d448e6d40a5e00625ebff9bbf
Winston Tsang <rubyconsumer@rubyconsumer.com>
2014-03-20 01:12:48 -0400

    commonize bundler command

---
+++ .bash/bundle.sh
@@ -0,0 +1,11 @@
+alias   bx="bundle exec"
+alias  tbe="time bundle exec"
+alias ttbe="time RAILS_ENV=test bundle exec"
+
+alias   bi="bundle install"
+
+alias   bu="bundle update"   # update the current environment
+alias   bo="bundle open"     # open source directory of the given bundled gem
+alias   bs="bundle show"     # shows all gems that are part of the bundle, or the path to a given gem
+alias bviz="bundle viz"      # generates a visual dependency graph
+
