79e39e57f0c2a307a82f9ecb797fcdd69e4ccd1a
Winston Tsang <rubyconsumer@rubyconsumer.com>
2014-03-20 01:34:12 -0400

    commonize ls related customizations.

---
+++ .bash/ls.sh
@@ -0,0 +1,19 @@
+# CUSTOMIZE COLORIZATION FOR LS
+export LS_COLORS='di=01;33'
+#export LSCOLORS=exfxcxdxbxegedabagacad  #DEFAULT
+export LSCOLORS=cxfxcxdxbxegedabagacad  #CUSTOM
+
+alias   ls='ls -Fq'
+alias  cls="clear;ls"
+
+alias  lsa="ls -aFq"
+alias clsa="clear;lsa"
+
+alias   ll="ls -l"
+alias  cll="clear;ll"
+
+alias  lla="ls -alFq"
+alias clla="clear;lla"
+
+alias dlsa="clear;date;echo;lsa"
+
