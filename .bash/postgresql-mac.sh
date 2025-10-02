6fc237d6132b494545269cd8ea76fb70edef4cab
Winston Tsang <winstont [at] umich [dot] edu>
2025-10-02 17:57:37 -0400

    consolidate postgres configs

---
+++ .bash/postgresql-mac.sh
@@ -0,0 +1,10 @@
+echo "postgresql-mac: loading"
+
+# Set up PGHOST to be localhost
+export PGHOST=localhost
+
+# Add postgres.app to path
+export PATH=/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH  # iMac
+
+# Add PostgreSQL 12 for 10x Application
+export PATH="/Library/PostgreSQL/12/bin/:$PATH"
